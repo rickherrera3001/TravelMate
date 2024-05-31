d2540bef1e8dee14353027951afb90d1f327fccc
rickherrera3001 <rihr.3001@gmail.com>
2024-05-31 13:35:38 -0600

    Initial Commit

---
+++ TravelMate/TravelMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelMateApp.swift
+//  TravelMate
+//
+//  Created by Ricardo Developer on 31/05/24.
+//
+
+import SwiftUI
+
+@main
+struct TravelMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
